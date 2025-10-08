c4597b7c4098fead5ceb5d43a8b0c0c3f9d4e678
Mech Mark <235818990+mech-mark@users.noreply.github.com>
2025-10-08 16:47:43 +1100

    preact greeting island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    // Add your site's URL here
-  site: "https://example.com"
+  // Add your site's URL here
+  site: "https://example.com",
+
+  integrations: [preact()]
 });
